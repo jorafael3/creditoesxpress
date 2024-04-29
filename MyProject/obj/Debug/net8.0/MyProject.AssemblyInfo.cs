14d74c5a2f58f8b8e839f7de18ab5a9ce01118ff
jorafael3 <jalvaradoe3@gmail.com>
2024-04-29 01:43:23 -0500

    zxc

--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503d8520ac62be132a1f1e7f505f14a017f14042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e973c893905b53e1ca476e3d7d29f22fff5f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
